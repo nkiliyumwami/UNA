30a41863a9401e980753db489db473ba3d92c6b9
Aline096 <anoclez@gmail.com>
2024-07-21 10:58:02 +0200

    Email subscription integration

---
+++ app/api/subscribe/route.ts
@@ -0,0 +1,35 @@
+import dbConnect from '../../../lib/dbConnect'
+import { NextResponse } from 'next/server'
+import EmailSubscriber from '@/lib/models/EmailSubscriber'
+
+export const maxDuration = 60
+
+export async function GET() {
+  await dbConnect()
+
+  try {
+    const emails = await EmailSubscriber .find({})
+    return NextResponse.json({ emails })
+  } catch (error) {
+    return NextResponse.json({ error: 'Failed to fetch email' }, { status: 500 })
+  }
+}
+
+export async function POST(request: Request) {
+  await dbConnect()
+
+  try {
+    const{ email} = await request.json()
+
+    const newEmail = new EmailSubscriber({ email })
+    await newEmail.save()
+    return NextResponse.json(newEmail, { status: 201 })
+  } catch (error) {
+    console.error(error)
+    return NextResponse.json(
+      { error: 'Email already exist' },
+      { status: 500 }
+    )
+  }
+}
+
